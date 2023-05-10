85a1033ffd6cccd44d2d6ddfc6f1374bd96e0019
Hyeonki Hong <hhk7734@gmail.com>
2023-05-11 02:49:43 +0900

    feat(app): add startup, shutdown, healthz

--- cmd/app/main.py
+++ cmd/app/main.py
@@ -1,11 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware import Middleware
 
 from internal.user_interface.restapi import middleware
 
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # Startup
+    yield
+    # Shutdown
+
+
 app = FastAPI(
+    lifespan=lifespan,
     middleware=[
         Middleware(middleware.Logger),
         Middleware(middleware.Recovery),
-    ]
+    ],
 )
+
+
+@app.get("/healthz")
+async def healthz():
+    return {"status": "ok"}
